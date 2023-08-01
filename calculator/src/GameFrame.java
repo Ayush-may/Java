1a9a3bb8cf971f104e4f7ef093bc56e2053d3976
Ayush Sharma <Ayushsharma014may@gmail.com>
2023-08-01 14:53:33 +0530

    added

---
+++ calculator/src/GameFrame.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+public class GameFrame extends JFrame {
+    GameFrame(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.add(new GamePanel());
+		this.setResizable(false);
+		this.setSize(400,600);
+		this.setLocation(200,50);
+		this.setTitle("Calculator");
+		this.setVisible(true);
+    }
+}
---
+++ calculator/src/GamePanel.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.Graphics;
+
+public class GamePanel extends JPanel {
+    
+    
+    GamePanel(){
+        this.setLayout(null);
+    }
+
+    public void paint(Graphics g){
+        super.paint(g);
+    }
+}
