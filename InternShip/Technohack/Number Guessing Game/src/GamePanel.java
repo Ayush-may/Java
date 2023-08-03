025f4c3bffb2a1c1383be35bf732afb7d7248601
Ayush Sharma <Ayushsharma014may@gmail.com>
2023-08-03 10:43:40 +0530

    #

--- InternShip/Technohack/Number Guessing Game/src/GamePanel.java
+++ InternShip/Technohack/Number Guessing Game/src/GamePanel.java
@@ -1,15 +1,110 @@
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Random;
+
 import javax.swing.*;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements KeyListener, ActionListener{
 
     final int SCREEN_WIDTH = 400;
     final int SCREEN_HEIGHT = 200;
 
+    int limit = 5;
+    int guessNum;
+    int inputNum;
+
+    JLabel gameName = new JLabel("Number Guessing Game");
+    JLabel outPutTxt = new JLabel("Sample Text");
+    JLabel limitTxt = new JLabel("Limit: " + limit);
+    JTextField numberArea = new JTextField("100");
+    JButton newGameBtn = new JButton("New Number");
+    JButton guessBtn = new JButton("Guess");
+
+    Random random = new Random();
+
     GamePanel(){
+        guessNum = random.nextInt(100);
         this.setLayout(null);
         this.setPreferredSize(new Dimension(SCREEN_WIDTH,SCREEN_HEIGHT));
+        
+        this.add(gameName);
+        this.add(outPutTxt);
+        this.add(numberArea);
+        this.add(newGameBtn);
+        this.add(guessBtn);
+        this.add(limitTxt);
 
+        gameName.setFont(new Font("Arial", Font.BOLD, 30));
+
+        gameName.setBounds(30, 10, 400, 30);
+        numberArea.setBounds(180, 50, 50, 30);
+        limitTxt.setBounds(183, 80, 50, 30);
+        guessBtn.setBounds(90, 110, 110, 30);
+        newGameBtn.setBounds(210, 110, 110, 30);
+        outPutTxt.setBounds(150,150,250,30);
+
+        guessBtn.addActionListener(this);
+        newGameBtn.addActionListener(this);
+        numberArea.addKeyListener(this);
+    }
+
+    public void paint(Graphics g){
+        super.paint(g);
+    }
+
+    public void checkNum() {
+        if( inputNum == guessNum ){
+            System.out.println("Win!!!");
+        }
+        else if(inputNum > guessNum){
+            System.out.println("input is higher than guesss number");
+        }
+        else{
+            System.out.println("input is lower than guess Number");
+        }
+    }
+
+    @Override
+    public void keyPressed(KeyEvent arg0) {
+        if(arg0.getKeyChar() == '\n'){
+            System.out.println("Enter");
+            numberArea.setText("");
+            checkNum();
+        }
+        // System.out.println(arg0.getKeyCode());
+    }
+
+    @Override
+    public void keyReleased(KeyEvent arg0) {
 
     }
+
+    @Override
+    public void keyTyped(KeyEvent arg0) {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent arg0) {
+        if(arg0.getSource() == guessBtn){
+            try {
+                inputNum = Integer.parseInt(numberArea.getText());
+                numberArea.setText("");
+                System.out.println(inputNum);
+                checkNum();
+            } catch (Exception e) {}
+        }
+        else if(arg0.getSource() == newGameBtn){
+            guessNum = random.nextInt(100);
+            System.out.println(guessNum);
+        }
+    }
+
+
 }
