9fb36a61822a463236cb22ff5f3831a5579cdd37
Ayush Sharma <Ayushsharma014may@gmail.com>
2023-08-05 14:50:26 +0530

    #

---
+++ InternShip/Technohack/Random Password Generator/src/RandomPasswordGenerator.java
@@ -0,0 +1,5 @@
+class RandomPasswordGenerator{
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ JDBC Works/practice-1/src/CreateDB.java
@@ -0,0 +1,5 @@
+public class CreateDB {
+    public static void main(String[] args) {
+        
+    }    
+}
